5196db77474652672cc83b6fb4840e66d87f1ac4
Laurent <lmazare@gmail.com>
2020-01-29 00:58:20 +0000

    Add a decorator example.

---
+++ example/test.py
@@ -0,0 +1,43 @@
+import ast, inspect, json, textwrap, types
+
+def node_to_python(node):
+  if not isinstance(node, ast.AST):
+    raise ValueError('not an ast node ' + str(type(node)))
+  fields = {}
+  for name, value in ast.iter_fields(node):
+    if isinstance(value, ast.AST):
+      value = node_to_python(value)
+    elif isinstance(value, list):
+      value = [node_to_python(v) for v in value]
+    fields[name] = value
+  return (node.__class__.__name__, fields)
+
+class PyAst(object):
+  def __init__(self, fn):
+    filename = inspect.getsourcefile(fn)
+    lines, lineno = inspect.getsourcelines(fn)
+    code = textwrap.dedent(''.join(lines[1:]))
+    tree = ast.parse(code)
+    self._tree = node_to_python(tree)
+    self._fn = fn
+    self._filename = filename
+    self._lineno = lineno
+
+  def __call__(self, *args, **kwargs):
+    return self._fn(*args, **kwargs)
+
+  def write(self, fobj):
+    json.dump(self._tree, fobj)
+
+def pyast(fn):
+  if not isinstance(fn, types.FunctionType):
+    raise ValueError('not a function')
+  return PyAst(fn)
+
+@pyast
+def test(x):
+  return x + 1
+
+with open('/tmp/test.json', 'w') as fobj:
+  test.write(fobj)
+
